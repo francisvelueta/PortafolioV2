556e76f3c5bd6bb2358459c92bca03d063304a5e
francisvelueta <fvelueta@outlook.com>
2019-01-14 11:29:14 -0600

    add domain

---
+++ precache-manifest.0d5dccef81f982226702830e888538db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "54afa65d4c3750d4c6af583293be79f7",
+    "url": "/static/media/perfil.54afa65d.jpeg"
+  },
+  {
+    "revision": "284332f518c6a66d89372fd973f6f3cb",
+    "url": "/static/media/logogeek.284332f5.png"
+  },
+  {
+    "revision": "da1f326682c64acacb9edcc49c33d116",
+    "url": "/static/media/CVFrancis.da1f3266.pdf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "49c7fccbc6e0e00f0ba2",
+    "url": "/static/js/main.49c7fccb.chunk.js"
+  },
+  {
+    "revision": "4acfd889887e7af47067",
+    "url": "/static/js/1.4acfd889.chunk.js"
+  },
+  {
+    "revision": "49c7fccbc6e0e00f0ba2",
+    "url": "/static/css/main.34e33259.chunk.css"
+  },
+  {
+    "revision": "4acfd889887e7af47067",
+    "url": "/static/css/1.51551317.chunk.css"
+  },
+  {
+    "revision": "75e9d5ac032b7ef4f15e1793aa2284bf",
+    "url": "/index.html"
+  }
+];

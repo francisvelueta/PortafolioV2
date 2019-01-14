993c47c051eb4ba3c18462d0b6b0658527833da7
francisvelueta <fvelueta@outlook.com>
2019-01-14 03:27:02 -0600

    Updates

---
+++ precache-manifest.eca3a0589d6cd547e74e50d5ca712563.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "54afa65d4c3750d4c6af583293be79f7",
+    "url": "/PortafolioV2/static/media/perfil.54afa65d.jpeg"
+  },
+  {
+    "revision": "284332f518c6a66d89372fd973f6f3cb",
+    "url": "/PortafolioV2/static/media/logogeek.284332f5.png"
+  },
+  {
+    "revision": "da1f326682c64acacb9edcc49c33d116",
+    "url": "/PortafolioV2/static/media/CVFrancis.da1f3266.pdf"
+  },
+  {
+    "revision": "8e7a20b8eb8d05cd8c5b",
+    "url": "/PortafolioV2/static/js/runtime~main.8e7a20b8.js"
+  },
+  {
+    "revision": "8ae2c436da01b867f53d",
+    "url": "/PortafolioV2/static/js/main.8ae2c436.chunk.js"
+  },
+  {
+    "revision": "4acfd889887e7af47067",
+    "url": "/PortafolioV2/static/js/1.4acfd889.chunk.js"
+  },
+  {
+    "revision": "8ae2c436da01b867f53d",
+    "url": "/PortafolioV2/static/css/main.34e33259.chunk.css"
+  },
+  {
+    "revision": "4acfd889887e7af47067",
+    "url": "/PortafolioV2/static/css/1.51551317.chunk.css"
+  },
+  {
+    "revision": "0b479a7d08a4b144c0c3d906ec8f04f9",
+    "url": "/PortafolioV2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f7bfa3763d0788718542c6a9861e2b3d.js"
+  "/PortafolioV2/precache-manifest.eca3a0589d6cd547e74e50d5ca712563.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/PortafolioV2/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
